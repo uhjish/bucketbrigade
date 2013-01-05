bd6fa5ab6b8128a3d21134d8cebad36884a03f5a
Ajish George <ajishg@gmail.com>
2013-01-04 21:53:18 -0500

    added info for pypi

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     version = "0.1",
     packages = find_packages(),
     scripts = ['b2b.py'],
-    install_requires = ['boto>=1.9']
+    install_requires = ['boto>=1.9'],
+    package_date = {
+            '':['*.job','*.md']
+            },
+    author="Ajish George"
+    author_email="root@rootedinsights.com"
+    description="A simple utility to allow syncing/copying S3 bucket files across AWS accounts."
+    url="http://github.com/rootedinsights/bucketbrigade"
 )
